112d375ed5882098e1ff299981c42bfd1739d623
MINSK\Stanislau_Tamashou <Stanislau_Tamashou@epam.com>
2022-05-31 17:42:35 +0300

    added new functionality in fs folder

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,10 @@
+import fs from 'fs';
+
 export const list = async () => {
-    // Write your code here 
+  const folderPath = 'src/fs/files';
+  if(fs.existsSync(folderPath)){
+    console.log(fs.readdirSync(folderPath, () => {}));
+  } else {
+    console.error('\x1b[31m%s\x1b[0m', new Error('FS operation failed'));
+  }
 };
